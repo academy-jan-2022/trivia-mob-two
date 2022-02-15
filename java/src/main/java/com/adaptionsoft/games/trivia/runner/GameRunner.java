40d72790f466f330d0dda87407b2252e8cbed8b8
oawood93 <oa.wood@outlook.com>
2022-02-15 15:57:04 +0000

    initial setup

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,5 +1,6 @@
 
 package com.adaptionsoft.games.trivia.runner;
+import java.io.*;
 import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
@@ -9,28 +10,46 @@ public class GameRunner {
 
 	private static boolean notAWinner;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+
+		File outputFile =
+				new File( "snapshots\\1\\output.txt");
+		outputFile.createNewFile();
+
+		PrintStream out = new PrintStream(outputFile);
+		System.setOut(out);
+
+		File csvFile =
+				new File( "snapshots\\1\\csv.txt");
+		outputFile.createNewFile();
+		PrintWriter csvOut = new PrintWriter(csvFile);
+
+
 		Game aGame = new Game();
-		
+
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
+
 		Random rand = new Random();
-	
+
+
 		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
+			var roll = rand.nextInt(5) + 1;
+			aGame.roll(roll);
 			
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
+			csvOut.println(roll + "," + notAWinner);
 		} while (notAWinner);
+
+		csvOut.close();
+
+
 		
 	}
 }
