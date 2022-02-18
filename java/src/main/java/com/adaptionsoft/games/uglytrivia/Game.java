9f5b0ba50ac0c3d20babd0f3ab33b7754010823e
iagardi <garbigz@gmail.com>
2022-02-18 11:43:38 +0000

    refactor

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -4,48 +4,12 @@ import java.util.*;
 
 
 public class Game {
-
-    public class Questions {
-        Hashtable<String, LinkedList<String>> questions;
-
-        public Questions() {
-
-            LinkedList<String> popQuestions = new LinkedList<>();
-            LinkedList<String> scienceQuestions = new LinkedList<>();
-            LinkedList<String> sportsQuestions = new LinkedList<>();
-            LinkedList<String> rockQuestions = new LinkedList<>();
-            for (int i = 0; i < 50; i++) {
-                popQuestions.addLast("Pop Question " + i);
-                scienceQuestions.addLast(("Science Question " + i));
-                sportsQuestions.addLast(("Sports Question " + i));
-                rockQuestions.addLast("Rock Question " + i);
-            }
-            questions = new Hashtable<>();
-            questions.put("Pop", popQuestions);
-            questions.put("Science", scienceQuestions);
-            questions.put("Sports", sportsQuestions);
-            questions.put("Rock", rockQuestions);
-        }
-    }
-
-    Questions questions = new Questions();
-
     public static final int MAX_PLACES = 12;
+    Questions questions = new Questions();
     List<Player> players = new ArrayList<>();
-
-//    LinkedList<String> popQuestions = new LinkedList<>();
-//    LinkedList<String> scienceQuestions = new LinkedList<>();
-//    LinkedList<String> sportsQuestions = new LinkedList<>();
-//    LinkedList<String> rockQuestions = new LinkedList<>();
-
     int currentPlayerIndex = 0;
     Player currentPlayer;
-
     boolean isGettingOutOfPenaltyBox;
-
-    public Game() {
-
-    }
 
     public void add(String playerName) {
         Player newPlayer = new Player(playerName);
@@ -111,7 +75,6 @@ public class Game {
             System.out.println(questions.questions.get("Rock").removeFirst());
     }
 
-
     private String currentCategory() {
         if (categoryTurn() == 0) return "Pop";
         if (categoryTurn() == 1) return "Science";
@@ -161,7 +124,6 @@ public class Game {
     private int nextPlayer() {
         return (currentPlayerIndex + 1) % players.size();
     }
-
 
     private boolean didPlayerWin() {
         return currentPlayer.purse != 6;
---
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Questions.java
@@ -0,0 +1,27 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import java.util.Hashtable;
+import java.util.LinkedList;
+
+public class Questions {
+	Hashtable<String, LinkedList<String>> questions;
+
+	public Questions() {
+
+		LinkedList<String> popQuestions = new LinkedList<>();
+		LinkedList<String> scienceQuestions = new LinkedList<>();
+		LinkedList<String> sportsQuestions = new LinkedList<>();
+		LinkedList<String> rockQuestions = new LinkedList<>();
+		for (int i = 0; i < 50; i++) {
+			popQuestions.addLast("Pop Question " + i);
+			scienceQuestions.addLast(("Science Question " + i));
+			sportsQuestions.addLast(("Sports Question " + i));
+			rockQuestions.addLast("Rock Question " + i);
+		}
+		questions = new Hashtable<>();
+		questions.put("Pop", popQuestions);
+		questions.put("Science", scienceQuestions);
+		questions.put("Sports", sportsQuestions);
+		questions.put("Rock", rockQuestions);
+	}
+}
